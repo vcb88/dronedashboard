73c29b0d44fb8f5a846af1a0d4153997721ca68a
Slava Karpizin <slava@getcellar.co>
2025-11-29 19:18:28 +0300

    feat: refactor CI/CD to use pre-built images and docker-compose.prod.yml

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,7 @@
 set -e
 
 echo "--- Deploying dronedashboard ---"
-echo "$GHCR_PAT" | docker login ghcr.io -u "$GITHUB_ACTOR" --password-stdin
 
-docker-compose -f docker-compose.prod.yml pull
 docker-compose -f docker-compose.prod.yml up -d --remove-orphans
 
 echo "--- Deployment of dronedashboard complete ---"
