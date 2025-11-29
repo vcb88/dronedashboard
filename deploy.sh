c24b2abac9172b986b011f0990db5f384bf0eb1a
Slava Karpizin <slava@getcellar.co>
2025-11-29 21:03:47 +0300

    Revert "fix: update deploy.sh to force pull and recreate containers and deploy"
    This reverts commit 9190bd65d123254ffbc8669090436483a9089a18.

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ export DOCKER_USERNAME
 
 echo "--- Deploying dronedashboard ---"
 
-docker compose -f docker-compose.prod.yml up -d --remove-orphans --pull always --force-recreate
+docker compose -f docker-compose.prod.yml up -d --remove-orphans
 
 echo "--- Deployment of dronedashboard complete ---"
