a8d1d60c9a9d67b71b35de87d73dd4e0cc6ae50a
Slava Karpizin <slava@getcellar.co>
2025-11-29 18:47:49 +0300

    feat: separate docker-compose for production deployment

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 echo "--- Deploying dronedashboard ---"
 echo "$GHCR_PAT" | docker login ghcr.io -u "$GITHUB_ACTOR" --password-stdin
 
-docker-compose -f docker-compose.yml pull
-docker-compose -f docker-compose.yml up -d --remove-orphans
+docker-compose -f docker-compose.prod.yml pull
+docker-compose -f docker-compose.prod.yml up -d --remove-orphans
 
 echo "--- Deployment of dronedashboard complete ---"
